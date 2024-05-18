ee6d604903f4a289b2caf9793e0650917ab33109
ravshanmahmudov <ravshanmahmudov882@gmail.com>
2024-05-19 08:13:45 +1400

    misolar

---
+++ js/index.js
@@ -0,0 +1,199 @@
+// 1chi masala
+
+// alert('son kiriting');
+// let a = +prompt("");
+// if (a > 0) {
+//     a++;
+// } 
+// console.log (a);
+
+// 2chi masala
+
+// alert('son kiriting');
+// let a = +prompt("");
+// if (a > 0) {
+//     a++;
+// }
+// else {
+// a-=2;
+// }
+// console.log (a);
+
+
+// 4chi masala
+
+// alert('3ta  SON KIRITING');
+// let k =0
+// let b = +prompt("b");
+// if (b > 0) {
+//     k++;
+// }
+// let a = +prompt("a");
+// if (a > 0) {
+//     k++;
+// }
+// let c = +prompt("c");
+// if (c > 0) {
+//     k++;
+// }
+// console.log (k);
+
+// 5chi masala
+
+// alert('3ta  SON KIRITING');
+// let k =0
+// let p =0
+// let b = +prompt("b");
+// if (b > 0) {
+//     k++;
+// }
+// else {
+// p--;
+// }
+// let a = +prompt("a");
+// if (a > 0) {
+//     k++;
+// }
+// else {
+//     p--;
+//     }
+// let c = +prompt("c");
+// if (c > 0) {
+//     k++;
+// }
+// else {
+//     p--;
+// }
+// console.log (k);
+// console.log (p);
+
+// 6chi masal
+
+// let p =0
+// alert('2ta  SON KIRITING');
+// let a = +prompt ("a")
+// let b = +prompt ("b")
+// if (a >! b) {
+//     p++;
+// } 
+
+
+
+
+// 8 - masala
+// function kattasiKichigi( g, b) {
+//     if (a > b) {
+//         console.log(g);
+//         console.log(b);
+//     } else {
+//         console.log(g);
+//         console.log(a);
+//     }
+// }
+// kattasiKichigi(16, 8); // Misol uchun: 16 va 8
+
+// 9 - masala 
+
+// let A = 5;
+// let B = 10;
+// if (A > B) {
+//     let temp = A;
+//     A = B;
+//     B = temp;
+// }
+// console.log("A qiymati: " + A);
+console.log("B qiymati: " + B);
+
+// 10 - masala
+
+// let A = 10;
+// let B = 5;
+// if (A !== B) {
+//   let sum = A + B;
+//   A = sum;
+//   B = sum;
+// } else {
+//   A = 0;
+//   B = 0;
+// }
+// console.log(A qiymati: ${A});
+// console.log(B qiymati: ${B});
+
+// 11 - masala
+
+// let A = 5;
+// let B = 3;
+// if (A !== B) {
+//     if (A > B) {
+//         console.log("A ning qiymati katta");
+//     } else {
+//         console.log("B ning qiymati katta");
+//     }
+// } else {
+//     A = 0;
+//     B = 0;
+//     console.log("A va B tengson.");
+// }
+// console.log("A = " + A);
+// console.log("B = " + B);
+
+// 12 - masala
+
+// function findSmallestNumber(num1, num2, num3) {
+//     let smallest = num1;
+//     if (num2 < smallest) {
+//       smallest = num2;
+//     }
+//     if (num3 < smallest) {
+//       smallest = num3;
+//     }
+//     return smallest;
+//   }
+//   let num1 = 10;
+//   let num2 = 5;
+//   let num3 = 8;
+//   let smallestNumber = findSmallestNumber(num1, num2, num3);
+//   console.log("The smallest number is: " + smallestNumber); 
+
+// 13 - masala
+
+// function findMiddleNumber(num1, num2, num3) {
+
+    // Sort the numbers in ascending order
+
+    // let sortedNumbers = [num1, num2, num3].sort((a, b) => a - b); 
+  
+    // The middle number is now at index 1
+
+//     return sortedNumbers[1]; 
+//   }
+//   let num1 = 10;
+//   let num2 = 5;
+//   let num3 = 8;
+//   let middleNumber = findMiddleNumber(num1, num2, num3);
+//   console.log("The middle number is: " + middleNumber);
+
+// 14 - masala
+
+// function findSmallestAndLargest(num1, num2, num3) {
+//     let smallest = Math.min(num1, num2, num3);
+//     let largest = Math.max(num1, num2, num3);
+//     console.log("The smallest number is: " + smallest);
+//     console.log("The largest number is: " + largest);
+//   }
+//   let num1 = 10;
+//   let num2 = 5;
+//   let num3 = 8;
+//   findSmallestAndLargest(num1, num2, num3);
+
+// 14 - masala
+
+// function findTwoLargestNumbers(num1, num2, num3) {
+//     let sortedNumbers = [num1, num2, num3].sort((a, b) => a - b); 
+//     console.log("The two largest numbers are: " + sortedNumbers[1] + " and " + sortedNumbers[2]);
+//   }
+//   let num1 = 10;
+//   let num2 = 5;
+//   let num3 = 8;
+//   findTwoLargestNumbers(num1, num2, num3);
+
